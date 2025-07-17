f12ecc66e306fdf495ef28e3ea0c8ec80280ecaf
vaceslav <irbis19822801@gmail.com>
2025-07-17 12:57:03 +0400

    В generate изменил функцию генерации ответа на асинхронную

--- app/generate.py
+++ app/generate.py
@@ -1,24 +1,23 @@
-from openai import OpenAI
+from openai import AsyncOpenAI
 from config import API_KEY, API_KEY2
 
-client = OpenAI(
+client = AsyncOpenAI(
   base_url="https://openrouter.ai/api/v1",
   api_key=API_KEY2,
 )
 
-completion = client.chat.completions.create(
-  # extra_headers={
-  #   "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
-  #   "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
-  # },
-  extra_body={},
-  model="deepseek/deepseek-chat-v3-0324:free",
-  messages=[
-    {
-      "role": "user",
-      "content": "Привет",
-      "temterature": "0.0"
-    }
-  ]
-)
-print(completion.choices[0].message.content)
+
+async def generate(text: str):
+    completion = await client.chat.completions.create(
+        extra_body={},
+        model="deepseek/deepseek-chat-v3-0324:free",
+        messages=[
+            {
+                "role": "user",
+                "content": "Привет",
+                "temterature": "0.0"
+            }
+        ]
+      )
+    print(completion)
+    return completion.choices[0].message.content
